22eab4c1b735f02ac7588878b7be2dd0fd22cd8a
Michael Boerman <michaelboerman@hey.com>
2025-08-08 10:38:48 -0400

    forgot to add function names to list in init.

--- here/__init__.py
+++ here/__init__.py
@@ -1,3 +1,3 @@
-from .here import get_root_directory, here
+from .here import get_root_directory, get_calling_script_file_path, here
 
-__all__ = ["get_root_directory", "here"]
+__all__ = ["get_root_directory", "get_calling_script_file_path", "here"]
